eebc876cdbbb46e45d060692abbff929bfb203f4
2021MD <88902897+2021MD@users.noreply.github.com>
2022-01-30 23:58:45 +0100

    Personensuche

---
+++ HomeController.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication7.Data;
+using WebApplication7.Models;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;      
+        }
+
+        public IActionResult Index()
+        {
+            var people = _context.People.ToList();
+            return View(people);
+        }
+
+        public IActionResult Details(int? id)
+        {
+            if( id == null )
+            {
+                return NotFound();
+            }
+
+            var person = _context.People.SingleOrDefault(
+                p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // GET: /Home/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Home/Create
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            _context.People.Add(person);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Home/Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = _context.People.SingleOrDefault(
+                p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: /Home/Edit
+        [HttpPost]
+        public IActionResult Edit(Person person)
+        {
+            _context.Update(person);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        // GET: /Home/Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var person = _context.People.SingleOrDefault(
+                p => p.Id == id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return View(person);
+        }
+
+        // POST: /Home/Delete
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var person = _context.People.SingleOrDefault(
+                p => p.Id == id);
+            _context.People.Remove(person);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+        public DateTime Geburtsdatum { get; set; }
+    }
+}
